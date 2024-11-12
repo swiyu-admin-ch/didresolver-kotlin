37d06043e9c29199d266771a3cfcfe5b73e763b3
Vladica-Stojic-BIT <vladica.stojic@bit.admin.ch>
2024-11-12 16:49:43 +0100

    :sparkles: feat: Upgrade files to version 0.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 group =
  "ch.admin.eid"
-version = "0.0.4"
+version = "0.0.5"
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -27,7 +27,7 @@ publishing {
         register<MavenPublication>("gpr") {
             groupId = "ch.admin.eid"
             artifactId = "didresolver"
-            version = "0.0.4"
+            version = "0.0.5"
             from(components["kotlin"])
         }
     }
